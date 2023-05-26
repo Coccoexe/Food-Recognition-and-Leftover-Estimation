2e6b1288b8ed559ca340a15ecaa7a371f37ff53a
akappakappa <20224688+akappakappa@users.noreply.github.com>
2023-05-26 16:37:24 +0200

    Add project files.

---
+++ Food-Recognition-and-Leftover-Estimation.cpp
@@ -0,0 +1,12 @@
+﻿// Food-Recognition-and-Leftover-Estimation.cpp : Defines the entry point for the application.
+//
+
+#include "Food-Recognition-and-Leftover-Estimation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
