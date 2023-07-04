8aaddc156e1288a175bce2fe675c50c87d77b792
akappakappa <andreavalde36@gmail.com>
2023-07-05 00:27:06 +0200

    blur and closing for segmentation

--- src/Mask.cpp
+++ src/Mask.cpp
@@ -6,19 +6,17 @@ Mask::Mask(const cv::Mat i, const std::vector<std::pair<int, cv::Rect>> b)
 	cv::Mat segments = cv::Mat::zeros(source_image.size(), CV_8UC1);
 	for (int i = 0; i < bounding_boxes.size(); i++)
 	{   // GrabCut for each bounding box
-		cv::Mat window = cv::Mat::zeros(source_image.size(), CV_8UC1);
-		cv::Mat bgdModel, fgdModel;
-		cv::grabCut(source_image, window, bounding_boxes[i].second, bgdModel, fgdModel, 3, cv::GC_INIT_WITH_RECT);
-		cv::compare(window, cv::GC_PR_FGD, window, cv::CMP_EQ);
-		//cv::medianBlur(window, window, 3);
-		cv::erode(window, window, cv::Mat(), cv::Point(-1, -1), 3);
-		cv::dilate(window, window, cv::Mat(), cv::Point(-1, -1), 3);
-		cv::dilate(window, window, cv::Mat(), cv::Point(-1, -1), 3);
-		cv::erode(window, window, cv::Mat(), cv::Point(-1, -1), 3);
-		cv::add(segments, window * bounding_boxes[i].first / 13, segments);
+		cv::Mat window = cv::Mat::zeros(source_image.size(), CV_8UC1);                                                // Mask for the current bounding box
+		cv::Mat bgdModel, fgdModel;                                                                                   // Background and foreground models
+		cv::grabCut(source_image, window, bounding_boxes[i].second, bgdModel, fgdModel, 3, cv::GC_INIT_WITH_RECT);    // GrabCut
+		cv::compare(window, cv::GC_PR_FGD, window, cv::CMP_EQ);                                                       // Keep only the foreground
+		cv::medianBlur(window, window, 5);                                                                            // Median blur to remove noise and fill holes
+		cv::morphologyEx(window, window, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5))); // Morphological closing to fill holes
+		cv::add(segments, window * bounding_boxes[i].first / 13, segments);                                           // Add the current mask to the final mask
 	}
 	cv::imshow("segments", segments);
 	cv::waitKey(0);
 }
 
 Mask::~Mask(){}
+
