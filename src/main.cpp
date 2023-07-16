854d211893f71fe270cff1dfb9af7d3e8a0dd7b0
akappakappa <andreavalde36@gmail.com>
2023-07-16 19:48:02 +0200

    cutouts

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,12 @@
 #include "BoundingBoxes.hpp"
 
 #include <string>
+#include <filesystem>
 #include <vector>
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+
 
 #define DEBUG true
 
@@ -23,14 +26,38 @@ int main()
 	const string DATASET_PATH = "./Food_leftover_dataset/";
 	const int NUMBER_OF_TRAYS = 8;
 	const vector<string> IMAGE_NAMES = { "food_image", "leftover1", "leftover2", "leftover3" };
+	auto cutout = [](const cv::Mat& image, const cv::Vec3f& circle) -> cv::Mat
+	{
+		const int x = cvRound(circle[0] - circle[2]) > 0 ? cvRound(circle[0] - circle[2]) : 0;
+		const int y = cvRound(circle[1] - circle[2]) > 0 ? cvRound(circle[1] - circle[2]) : 0;
+		const int w = x + cvRound(2 * circle[2]) < image.cols ? cvRound(2 * circle[2]) : image.cols - x;
+		const int h = y + cvRound(2 * circle[2]) < image.rows ? cvRound(2 * circle[2]) : image.rows - y;
+		return image(cv::Rect(x, y, w, h));
+	};
 
-	// Cycle through all images
+	// Process
+	if (!filesystem::exists("./plates/")) filesystem::create_directory("./plates/");
 	for (int i = 1; i <= NUMBER_OF_TRAYS; i++)
 	{	// For each tray
+		if (!filesystem::exists("./plates/tray" + to_string(i) + "/")) filesystem::create_directory("./plates/tray" + to_string(i) + "/");
 		for (const auto& imgname : IMAGE_NAMES)
 		{	// For each image
+			if (!filesystem::exists("./plates/tray" + to_string(i) + "/" + imgname + "/")) filesystem::create_directory("./plates/tray" + to_string(i) + "/" + imgname + "/");
 			cv::Mat image = cv::imread(DATASET_PATH + "tray" + to_string(i) + "/" + imgname + ".jpg");
 			BoundingBoxes bb(image);
+			vector<cv::Vec3f> plates = bb.getPlates();
+			pair<bool, cv::Vec3f> salad = bb.getSalad();
+			pair<bool, cv::Rect> bread = bb.getBread();
+
+			// Save plates cutouts
+			for (int j = 0; j < plates.size(); j++)
+			{
+				cv::imwrite("./plates/tray" + to_string(i) + "/" + imgname + "/plate" + to_string(j) + ".jpg", cutout(image, plates[j]));
+				// run CLIP
+				// get k classes:
+				//     if k == 1 --> grabCut segmentation
+				//     if k > 1  --> k+1 means segmentation
+			}
 		}
 	}
 
